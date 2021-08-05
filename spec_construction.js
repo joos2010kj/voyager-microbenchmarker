dcf37ada25a05341afc4f10ae0625117dd626430
siyaoL1 <siyaoli@terpmail.umd.edu>
2021-08-05 00:34:24 -0400

    Added facet to the vega-lite spec

--- spec_construction.js
+++ spec_construction.js
@@ -1,14 +1,22 @@
 const fs = require('fs')
 const data = require('./runtime.json');
 // const data = [
-//   {"vega": "1", "transformation": "filter", "runtime": 110}, 
-//   {"vega": "2", "transformation": "filter", "runtime": 130}, 
-//   {"vega": "1", "transformation": "aggregate", "runtime": 150},
-//   {"vega": "2", "transformation": "aggregate", "runtime": 120},
-//   {"vega": "1", "transformation": "bin", "runtime": 100},
-//   {"vega": "2", "transformation": "bin", "runtime": 170},
-//   {"vega": "1", "transformation": "extent", "runtime": 110},
-//   {"vega": "2", "transformation": "extent", "runtime": 110}
+//   {"vega": "1", "transformation": "filter", "runtime": 110, "size": "5k"}, 
+//   {"vega": "2", "transformation": "filter", "runtime": 130, "size": "5k"}, 
+//   {"vega": "1", "transformation": "aggregate", "runtime": 150, "size": "5k"},
+//   {"vega": "2", "transformation": "aggregate", "runtime": 120, "size": "5k"},
+//   {"vega": "1", "transformation": "bin", "runtime": 100, "size": "5k"},
+//   {"vega": "2", "transformation": "bin", "runtime": 170, "size": "5k"},
+//   {"vega": "1", "transformation": "extent", "runtime": 110, "size": "5k"},
+//   {"vega": "2", "transformation": "extent", "runtime": 110, "size": "5k"},
+//   {"vega": "1", "transformation": "filter", "runtime": 200, "size": "10k"}, 
+//   {"vega": "2", "transformation": "filter", "runtime": 230, "size": "10k"}, 
+//   {"vega": "1", "transformation": "aggregate", "runtime": 250, "size": "10k"},
+//   {"vega": "2", "transformation": "aggregate", "runtime": 220, "size": "10k"},
+//   {"vega": "1", "transformation": "bin", "runtime": 200, "size": "10k"},
+//   {"vega": "2", "transformation": "bin", "runtime": 370, "size": "10k"},
+//   {"vega": "1", "transformation": "extent", "runtime": 310, "size": "10k"},
+//   {"vega": "2", "transformation": "extent", "runtime": 210, "size": "10k"}
 // ];
 
 const spec = {
@@ -18,25 +26,27 @@ const spec = {
     "transform": [
       {"calculate": "datum.vega == 1 ? 'Vega' : 'Postgres'", "as": "application"}
     ],
-    "width": {"step": 12},
-    "mark": "bar",
-    "encoding": {
-      "column": {
-        "field": "transformation", "type": "nominal", 
-        "spacing": 10, "title": "Transformation"
-      },
-      "y": {
-        "aggregate": "sum", 
-        "field": "runtime",
-        "title": "Runtime(ms)",
-        "axis": {"grid": false}
-      },
-      "x": {
-        "field": "application",
-        "axis": {"title": ""}
-      },
-      "color": {
-        "field": "application"
+    "facet": {"column": {"field": "size", "title": "Dataset Size"}},
+    "spec": {
+      "mark": "bar",
+      "encoding": {
+        "x": {
+          "field": "application",
+          "axis": {"title": ""}
+        },
+        "y": {
+          "aggregate": "sum", 
+          "field": "runtime",
+          "title": "Runtime(ms)",
+          "axis": {"grid": false}
+        },
+        "column": {
+          "field": "transformation", "type": "nominal", 
+          "spacing": 10, "title": "Transformation"
+        },
+        "color": {
+          "field": "application"
+        }
       }
     },
     "config": {
